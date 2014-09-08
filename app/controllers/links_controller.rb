31765e603ba41ca96a9ccde4c8e40df446fb7254
Carolyn Huynh & Dustin Lewis <student@epicodus.com>
2014-09-08 11:42:23 -0700

    import bootstrap theme.

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,21 @@
+class LinksController < ApplicationController
+
+  def index
+    @links = Link.all
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(params[:link])
+    if @link.save
+      flash[:notice] = "#{link.description} has been created."
+      redirect_to links_path
+    else
+      render 'new'
+    end
+  end
+
+end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ActiveRecord::Base
   validates :description, :presence => true
   validates :user_id, :numericality => true
+
+  belongs_to :user
 end
