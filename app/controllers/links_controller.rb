0f13a74fec687825b1d4a7da38da35bcb0acd1ed
Justin Pacubas & Carolyn Huynh & Dustin Lewis <student@epicodus.com>
2014-09-09 14:54:00 -0700

    Implement score and sorting on index page.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,7 @@ class LinksController < ApplicationController
 
   def index
     @links = Link.all
+    # blah
     @vote = Vote.new
   end
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -5,6 +5,16 @@ class Link < ActiveRecord::Base
   belongs_to :user
   has_many :votes
 
+  def self.sort_by_score
+    self.all.sort { |a,b| a.link_score <=> b.link_score }.reverse
+  end
+
+  def link_score
+    created = self.created_at.strftime('%a, %d %b %Y').to_date
+    age = (Date.today - created).to_i
+    score = (self.votes.count) * 10 - age
+  end
+
   def upvote_total
     self.votes.count
   end
